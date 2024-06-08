38ed417c5ea2d30ba3da9df5e3160c8ec88544e8
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-06-08 10:32:16 -0700

    highlight OVERRIDE comments

---
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+-- Highlight todo, notes, etc in comments
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    signs = true, -- show icons in gutter
+    sign_priority = 0, -- don't overlay gitsigns
+    keywords = {
+      OVERRIDE = { icon = '󰬲 ', color = 'hint', alt = { 'OVERRIDE FILE' } },
+    },
+  },
+}
