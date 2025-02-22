d7dc655012e53e8a97db7bb980b510c2003ee251
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-02-22 15:56:01 -0800

    basic Xeref functionality supported :tada:

--- .config/nvim/lua/xeref.lua
+++ .config/nvim/lua/xeref.lua
@@ -1,27 +1,13 @@
 local ts_utils = require 'nvim-treesitter.ts_utils'
 local parsers = require 'nvim-treesitter.parsers'
 
-local function copy_method_name()
+-- Copy the method reference the cursor is within to the system clipboard
+local function copy_method_ref()
   local bufnr = vim.api.nvim_get_current_buf()
 
-  if not parsers.get_buf_lang() == 'ruby' then
+  if parsers.get_buf_lang() ~= 'ruby' then
     print 'Xeref currently only supports Ruby'
-  end
-
-  local parser = vim.treesitter.get_parser(bufnr, 'ruby')
-  local tree = parser:parse()[1] -- Get the syntax tree
-
-  local class_or_module_query = '((method name: (identifier) @method.name)) \z
-                                 | ((singleton_method name: (identifier) @singleton_method.name))'
-  local class_or_module_captures = vim.treesitter.query.parse('ruby', class_or_module_query)
-
-  local class_and_module_nodes = {}
-  for _, node in class_or_module_captures:iter_captures(tree:root(), bufnr) do
-    table.insert(class_and_module_nodes, node)
-  end
-
-  for i, node in ipairs(class_and_module_nodes) do
-    print(i, vim.treesitter.get_node_text(node, bufnr))
+    return
   end
 
   -- Find the smallest treesitter node that contains the cursor
@@ -32,48 +18,50 @@ local function copy_method_name()
     return
   end
 
-  -- Traverse up the syntax tree to find the method definition node
+  -- Traverse up the syntax tree
+  local method_name
+  local class_and_namespaces = {}
   while node do
     local node_type = node:type()
     if node_type == 'method' or node_type == 'singleton_method' then
+      for child in node:iter_children() do
+        if child:type() == 'identifier' then
+          method_name = vim.treesitter.get_node_text(child, bufnr)
+        end
+      end
+    elseif node_type == 'class' or node_type == 'module' then
+      for child in node:iter_children() do
+        if child:type() == 'constant' then
+          table.insert(class_and_namespaces, vim.treesitter.get_node_text(child, bufnr))
+        elseif child:type() == 'scope_resolution' then -- Matches "module Foo::Bar"
+          for scope_child in child:iter_children() do
+            if scope_child:type() == 'constant' then
+              table.insert(class_and_namespaces, vim.treesitter.get_node_text(scope_child, bufnr))
+            end
+          end
+        end
+      end
+    end
+
+    if node:parent() == nil then
       break
     end
     node = node:parent()
   end
 
-  if not node then
+  if not method_name then
     print 'Cursor is not inside a method'
     return
   end
 
-  -- Traverse up the syntax tree to find the class and/or module node(s)
-  -- local ancestors = {}
-  -- while node do
-  --   local node_type = node:type()
-  --   if node_type == 'class' or node_type == 'module' then
-  --     table.insert(ancestors, value)
-  --     break
-  --   end
-  --   node = node:parent()
-  -- end
-  --
-  local query
-  if node:type() == 'method' then
-    query = vim.treesitter.query.parse('ruby', '((method name: (identifier) @method.name))')
-  else
-    query = vim.treesitter.query.parse('ruby', '((singleton_method name: (identifier) @singleton_method.name))')
-  end
+  -- GrandParent::Parent#method
+  local method_ref = table.concat(vim.fn.reverse(class_and_namespaces), '::') .. '#' .. method_name
 
-  local method_name
-  for _, subnode in query:iter_captures(node, bufnr) do
-    method_name = vim.treesitter.get_node_text(subnode, bufnr)
-  end
+  -- Copy the method ref to register synced with system clipboard
+  vim.fn.setreg('+', method_ref)
 
-  -- Copy the method name to register
-  vim.fn.setreg('+', method_name)
-
-  print('Copied ' .. method_name)
+  print('Copied ' .. method_ref)
 end
 
 -- Assign the function to a command
-vim.api.nvim_create_user_command('Xeref', copy_method_name, {})
+vim.api.nvim_create_user_command('Xeref', copy_method_ref, {})
