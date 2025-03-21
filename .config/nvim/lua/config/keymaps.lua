a151b3a73d9d344fe6d3c78fb99319bf12f66c94
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-03-21 15:27:46 -0700

    yanky keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>yfn", "<cmd>let @+ = expand('%:t')<cr>", { desc = "file [n]ame" })
+vim.keymap.set("n", "<leader>yfp", function()
+  local homePath = os.getenv("HOME")
+  local fullPath = vim.fn.expand("%")
+  -- Will match $HOME/Development/my-project/
+  local basePath = homePath .. "/Development/" .. "[^/]+/"
+  local relativePath = fullPath:gsub(basePath, "")
+
+  vim.fn.setreg("+", relativePath)
+  print("Copied " .. relativePath)
+end, { desc = "file [p]ath" })
+
+vim.keymap.set("n", "<leader>yr", "<cmd>Xeref<cr>", { desc = "[r]eference" })
