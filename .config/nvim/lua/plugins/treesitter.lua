20591fc5252d8d0c31a8909422aac86208184a9c
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-04-19 10:34:12 -0700

    update TS config and plugins

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,53 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "ruby" } },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+      "RRethy/nvim-treesitter-endwise",
+    },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "diff",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "toml",
+        "tsx",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
+      endwise = {
+        enable = true,
+      },
+      context = {
+        enable = true,
+        max_lines = 10,
+        multiline_threshold = 1,
+        -- trim_scope = 'outer',
+      },
+    },
   },
 }
