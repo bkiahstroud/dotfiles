bdd372f7f9b6580952ad705cc2150eb994408832
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-04-19 10:33:57 -0700

    add jk keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "v", "i" }, "jk", "<Esc>")
+vim.keymap.set({ "v", "i" }, "Jk", "<Esc>")
+vim.keymap.set({ "v", "i" }, "jK", "<Esc>")
+vim.keymap.set({ "v", "i" }, "JK", "<Esc>")
 
 vim.keymap.set("n", "<leader>yfn", "<cmd>let @+ = expand('%:t')<cr>", { desc = "file [n]ame" })
 vim.keymap.set("n", "<leader>yfp", function()
