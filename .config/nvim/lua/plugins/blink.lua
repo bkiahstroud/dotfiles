4906265e20dc05637db853445cf403633652aa65
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-04-21 10:14:33 -0700

    use super-tab blink.nvim keymap

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+    },
+    completion = {
+      trigger = {
+        show_in_snippet = false, -- recommended with super-tab preset
+      },
+    },
+  },
+}
