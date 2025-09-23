97446cda0d956512c087dd99dd3f9201ffab1f61
Kiah Stroud <kiah@kiah.dev>
2025-09-23 12:06:10 -0700

    disable autoformat in markdown files by default

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,3 +1,2 @@
 vim.opt_local.textwidth = 80
-vim.opt_local.formatoptions:append("a")
 vim.opt_local.spell = true
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,7 +7,6 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- TODO: add to ftplugin/markdown.lua?
 -- Disable autoformat for markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "markdown" },
