faa1fd0b57f5f5244b54a0feff6f16effd61a83e
Kiah Stroud <kiah@kiah.dev>
2026-08-18 13:59:39 -0700

    path and var configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,19 @@ if status is-interactive
 
   set -U fish_greeting
 
+  fish_add_path -g $HOME/.krew/bin
+
+  set -gx EDITOR (command -v nvim)
+  set -gx GPG_TTY (tty)
+  set -gx KUBECONFIG $HOME/.kube/config
+  set -gx CARGO_TARGET_DIR $HOME/.cargo/shared_target
+
+  fish_vi_key_bindings
+
+  if test -s $HOME/local.fish
+    source $HOME/local.fish
+  end
+
   zoxide init fish | source
   starship init fish | source
 end
